4f4dfdea2258289c41b274163146a04c7fb6ff78
zc_return0 <237350543@qq.com>
2019-11-26 09:26:53 +0800

    update

--- generator异步应用/获取接口数据.js
+++ generator异步应用/获取接口数据.js
@@ -1,21 +1,18 @@
 const fetch = require('node-fetch');
 const URL = 'https://api.github.com/users/ZCreturn0';
 
-// fetch(URL).then(res => {
-//     res.then(data => {
-//         console.log(data);
-//     })
-// });
-
 function* gen() {
     let result = yield fetch(URL);
+    console.log(result);
 }
 
 let g = gen();
 let result = g.next();
 result.value.then(res => {
+    // 返回 Promise
     return res.json();
 })
 .then(data => {
     console.log(data);
+    g.next(data);
 })
